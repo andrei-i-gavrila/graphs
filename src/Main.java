1620f7728404faa49e7f508a3bf8ea4c57271ae8
kiuny <kiuny@github.com>
2017-03-02 13:08:50 +0200

    Hello world

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kiuny on 02.03.2017.
+ * Main program entry point
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
